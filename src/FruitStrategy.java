218b9c9fe329733f3377748a4352a98325677c5e
IodizedSalt <christophermoeller2000@gmail.com>
2019-12-07 18:59:28 +0100

    carrot

--- src/FruitStrategy.java
+++ src/FruitStrategy.java
@@ -1,14 +1,24 @@
 import java.util.Scanner;
 
 public class FruitStrategy implements Strategy {
+
     @Override
     public Item addNewItem(Scanner scanner, Item item, String itemName) {
-        return null;
+
+
+        System.out.printf("How many %s\n", itemName);
+        int fruitAmount = scanner.nextInt();
+        item.setAmount(fruitAmount);
+
+        return item;
     }
 
     @Override
     public void addToExistingItem(Scanner scanner, Item item, String itemName, int currentAmount) {
-
+        System.out.printf("How many %s\n", itemName);
+        int fruitAmount = scanner.nextInt();
+        item.setAmount(currentAmount + fruitAmount);
     }
+
 
 }
--- src/SpiceStrategy.java
+++ src/SpiceStrategy.java
@@ -1,14 +1,23 @@
 import java.util.Scanner;
 
 public class SpiceStrategy implements Strategy{
+
     @Override
     public Item addNewItem(Scanner scanner, Item item, String itemName) {
-        return null;
+
+
+        System.out.printf("How much %s (g)\n", itemName );
+        int vegetableAmount = scanner.nextInt();
+        item.setAmount(vegetableAmount);
+
+        return item;
     }
 
     @Override
     public void addToExistingItem(Scanner scanner, Item item, String itemName, int currentAmount) {
-
+        System.out.printf("How many % (g)\n", itemName);
+        int vegetableAmount = scanner.nextInt();
+        item.setAmount(currentAmount + vegetableAmount);
     }
 
 }
